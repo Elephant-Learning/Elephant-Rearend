e0b6c62ec08d48c8a67bd1423549997df81aa663
OverlordsIII <OverlordsIII@users.noreply.github.com>
2022-07-28 15:03:45 -0700

    add security

---
+++ src/main/java/me/elephantsuite/security/config/WebSecurityConfig.java
@@ -0,0 +1,112 @@
+package me.elephantsuite.security.config;
+
+import me.elephantsuite.user.ElephantUserService;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.SecurityContextConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@AllArgsConstructor
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	private final ElephantUserService elephantUserService;
+
+	private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+	/**
+	 * Override this method to configure the {@link HttpSecurity}. Typically subclasses
+	 * should not invoke this method by calling super as it may override their
+	 * configuration. The default configuration is:
+	 *
+	 * <pre>
+	 * http.authorizeRequests().anyRequest().authenticated().and().formLogin().and().httpBasic();
+	 * </pre>
+	 * <p>
+	 * Any endpoint that requires defense against common vulnerabilities can be specified
+	 * here, including public ones. See {@link HttpSecurity#authorizeRequests} and the
+	 * `permitAll()` authorization rule for more details on public endpoints.
+	 *
+	 * @param http the {@link HttpSecurity} to modify
+	 * @throws Exception if an error occurs
+	 */
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.csrf()
+			.disable()
+			.authorizeRequests()
+			.antMatchers("/api/v*/registration/**")
+			.permitAll()
+			.anyRequest()
+			.authenticated()
+			.and()
+			.formLogin();
+	}
+
+	/**
+	 * Used by the default implementation of {@link #authenticationManager()} to attempt
+	 * to obtain an {@link AuthenticationManager}. If overridden, the
+	 * {@link AuthenticationManagerBuilder} should be used to specify the
+	 * {@link AuthenticationManager}.
+	 *
+	 * <p>
+	 * The {@link #authenticationManagerBean()} method can be used to expose the resulting
+	 * {@link AuthenticationManager} as a Bean. The {@link #userDetailsServiceBean()} can
+	 * be used to expose the last populated {@link UserDetailsService} that is created
+	 * with the {@link AuthenticationManagerBuilder} as a Bean. The
+	 * {@link UserDetailsService} will also automatically be populated on
+	 * {@link HttpSecurity#getSharedObject(Class)} for use with other
+	 * {@link SecurityContextConfigurer} (i.e. RememberMeConfigurer )
+	 * </p>
+	 *
+	 * <p>
+	 * For example, the following configuration could be used to register in memory
+	 * authentication that exposes an in memory {@link UserDetailsService}:
+	 * </p>
+	 *
+	 * <pre>
+	 * &#064;Override
+	 * protected void configure(AuthenticationManagerBuilder auth) {
+	 * 	auth
+	 * 	// enable in memory based authentication with a user named
+	 * 	// &quot;user&quot; and &quot;admin&quot;
+	 * 	.inMemoryAuthentication().withUser(&quot;user&quot;).password(&quot;password&quot;).roles(&quot;USER&quot;).and()
+	 * 			.withUser(&quot;admin&quot;).password(&quot;password&quot;).roles(&quot;USER&quot;, &quot;ADMIN&quot;);
+	 * }
+	 *
+	 * // Expose the UserDetailsService as a Bean
+	 * &#064;Bean
+	 * &#064;Override
+	 * public UserDetailsService userDetailsServiceBean() throws Exception {
+	 * 	return super.userDetailsServiceBean();
+	 * }
+	 *
+	 * </pre>
+	 *
+	 * @param auth the {@link AuthenticationManagerBuilder} to use
+	 * @throws Exception
+	 */
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.authenticationProvider(daoAuthenticationProvider());
+	}
+
+	@Bean
+	public DaoAuthenticationProvider daoAuthenticationProvider() {
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		provider.setPasswordEncoder(bCryptPasswordEncoder);
+		provider.setUserDetailsService(this.elephantUserService);
+
+		return provider;
+	}
+}
