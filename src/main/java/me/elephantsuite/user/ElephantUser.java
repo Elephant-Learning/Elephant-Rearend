9a6dedf1b0c68a30d4e519534fda75df8331ae03
OverlordsIII <OverlordsIII@users.noreply.github.com>
2022-07-28 15:12:16 -0700

    remove security stuff for now

--- src/main/java/me/elephantsuite/user/ElephantUser.java
+++ src/main/java/me/elephantsuite/user/ElephantUser.java
@@ -32,9 +32,6 @@ import me.elephantsuite.stats.ElephantUserStatistics;
 import me.elephantsuite.user.notification.Notification;
 import org.hibernate.annotations.Fetch;
 import org.hibernate.annotations.FetchMode;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 @Getter
 @Setter
@@ -44,7 +41,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 @Entity
 @ToString
 //eventually what we want to create based on a registration request
-public class ElephantUser implements UserDetails {
+public class ElephantUser {
 
 	@Id
 	@SequenceGenerator(name = "student_sequence", sequenceName = "student_sequence", allocationSize = 1)
@@ -109,83 +106,4 @@ public class ElephantUser implements UserDetails {
 		this.statistics = new ElephantUserStatistics(this);
 	}
 
-
-	/**
-	 * Returns the authorities granted to the user. Cannot return <code>null</code>.
-	 *
-	 * @return the authorities, sorted by natural key (never <code>null</code>)
-	 */
-	@Override
-	public Collection<? extends GrantedAuthority> getAuthorities() {
-		SimpleGrantedAuthority authority = new SimpleGrantedAuthority(this.type.name());
-
-		return Collections.singleton(authority);
-	}
-
-	/**
-	 * Returns the password used to authenticate the user.
-	 *
-	 * @return the password
-	 */
-	@Override
-	public String getPassword() {
-		return this.password;
-	}
-
-	/**
-	 * Returns the username used to authenticate the user. Cannot return
-	 * <code>null</code>.
-	 *
-	 * @return the username (never <code>null</code>)
-	 */
-	@Override
-	public String getUsername() {
-		return this.email;
-	}
-
-	/**
-	 * Indicates whether the user's account has expired. An expired account cannot be
-	 * authenticated.
-	 *
-	 * @return <code>true</code> if the user's account is valid (ie non-expired),
-	 * <code>false</code> if no longer valid (ie expired)
-	 */
-	@Override
-	public boolean isAccountNonExpired() {
-		return true;
-	}
-
-	/**
-	 * Indicates whether the user is locked or unlocked. A locked user cannot be
-	 * authenticated.
-	 *
-	 * @return <code>true</code> if the user is not locked, <code>false</code> otherwise
-	 */
-	@Override
-	public boolean isAccountNonLocked() {
-		return !this.locked;
-	}
-
-	/**
-	 * Indicates whether the user's credentials (password) has expired. Expired
-	 * credentials prevent authentication.
-	 *
-	 * @return <code>true</code> if the user's credentials are valid (ie non-expired),
-	 * <code>false</code> if no longer valid (ie expired)
-	 */
-	@Override
-	public boolean isCredentialsNonExpired() {
-		return true;
-	}
-
-	/**
-	 * Indicates whether the user is enabled or disabled. A disabled user cannot be
-	 * authenticated.
-	 *
-	 * @return <code>true</code> if the user is enabled, <code>false</code> otherwise
-	 */
-	@Override
-	public boolean isEnabled() {
-		return this.enabled;
-	}
 }
--- src/main/java/me/elephantsuite/user/ElephantUserService.java
+++ src/main/java/me/elephantsuite/user/ElephantUserService.java
@@ -11,9 +11,6 @@ import me.elephantsuite.registration.token.ConfirmationToken;
 import me.elephantsuite.registration.token.ConfirmationTokenService;
 import lombok.AllArgsConstructor;
 import org.springframework.lang.Nullable;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,32 +19,13 @@ import org.springframework.transaction.annotation.Transactional;
 @AllArgsConstructor
 @Transactional
 //loads data from user repository
-public class ElephantUserService implements UserDetailsService {
+public class ElephantUserService {
 
 	private final ElephantUserRepository elephantUserRepository;
 
 	private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
 	private final ConfirmationTokenService confirmationTokenService;
-
-	/**
-	 * Locates the user based on the username. In the actual implementation, the search
-	 * may possibly be case sensitive, or case insensitive depending on how the
-	 * implementation instance is configured. In this case, the <code>UserDetails</code>
-	 * object that comes back may have a username that is of a different case than what
-	 * was actually requested..
-	 *
-	 * @param username the username identifying the user whose data is required.
-	 * @return a fully populated user record (never <code>null</code>)
-	 * @throws UsernameNotFoundException if the user could not be found or the user has no
-	 *                                   GrantedAuthority
-	 */
-	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		return this.elephantUserRepository
-			.findByEmail(username)
-			.orElseThrow(() -> new UsernameNotFoundException("Could not find user with email " + username + " !"));
-	}
 
 	public ConfirmationToken signUpUser(ElephantUser user) {
 
