a047eae02fcd124f62341e62ea47e61b0c4ee039
OverlordsIII <OverlordsIII@users.noreply.github.com>
2022-07-28 15:52:12 -0700

    add cors mapping to allow everything

--- src/main/java/me/elephantsuite/ElephantBackendApplication.java
+++ src/main/java/me/elephantsuite/ElephantBackendApplication.java
@@ -5,6 +5,9 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ElephantBackendApplication {
@@ -23,4 +26,14 @@ public class ElephantBackendApplication {
 		SpringApplication.run(ElephantBackendApplication.class, args);
 	}
 
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
+	}
 }
