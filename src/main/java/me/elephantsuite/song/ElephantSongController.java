baae2212b047ff16857e529a872dc9f03a6d89fe
OverlordsIII <OverlordsIII@users.noreply.github.com>
2022-07-31 13:09:59 -0700

    add unlike song request

--- src/main/java/me/elephantsuite/song/ElephantSongController.java
+++ src/main/java/me/elephantsuite/song/ElephantSongController.java
@@ -2,6 +2,7 @@ package me.elephantsuite.song;
 
 import lombok.AllArgsConstructor;
 import me.elephantsuite.response.Response;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,5 +18,10 @@ public class ElephantSongController {
 	@PutMapping(path = "like")
 	public Response likeSong(@RequestBody SongRequest request) {
 		return songService.likeSong(request);
+	}
+
+	@DeleteMapping(path = "unlike")
+	public Response unlikeSong(@RequestBody SongRequest request) {
+		return songService.unlikeSong(request);
 	}
 }
