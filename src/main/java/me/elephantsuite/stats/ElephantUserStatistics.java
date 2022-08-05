891728db83e113b0b22117821190890c637872d4
OverlordsIII <OverlordsIII@users.noreply.github.com>
2022-08-04 17:01:53 -0700

    make user stats embedded

--- src/main/java/me/elephantsuite/stats/ElephantUserStatistics.java
+++ src/main/java/me/elephantsuite/stats/ElephantUserStatistics.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.Embeddable;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -22,27 +23,15 @@ import me.elephantsuite.user.ElephantUser;
 @EqualsAndHashCode
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 @NoArgsConstructor
-@Entity
 @ToString
+@Embeddable
 public class ElephantUserStatistics {
-	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "elephant_user_statistics_generator")
-	@SequenceGenerator(name = "elephant_user_statistics_generator", sequenceName = "elephant_user_statistics_sequence", allocationSize = 1)
-	private Long id;
 
 	private int daysStreak = 0;
 
 	private double usageTime = 0;
 
 	private LocalDateTime lastLoggedIn = LocalDateTime.now();
-
-	@OneToOne(mappedBy = "statistics")
-	@JsonBackReference
-	private ElephantUser user;
-
-	public ElephantUserStatistics(ElephantUser user) {
-		this.user = user;
-	}
 
 	public void incrementDaysStreak() {
 		if ((this.lastLoggedIn.getDayOfYear() + 1 == LocalDateTime.now().getDayOfYear()) && this.lastLoggedIn.getYear() == LocalDateTime.now().getYear()) {
