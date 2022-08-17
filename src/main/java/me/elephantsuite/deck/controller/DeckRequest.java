c5c149286b268aeaf62774f763814b4e607a2385
OverlordsIII <69648849+OverlordsIII@users.noreply.github.com>
2022-08-16 21:01:17 -0700

    remove set deck name bc i forgot it was alr there

--- src/main/java/me/elephantsuite/deck/controller/DeckRequest.java
+++ src/main/java/me/elephantsuite/deck/controller/DeckRequest.java
@@ -80,15 +80,4 @@ public class DeckRequest {
 
 		private final long sharedUserId;
 	}
-
-	@Getter
-	@AllArgsConstructor
-	@EqualsAndHashCode
-	@ToString
-	public static class SetName {
-
-		private final long deckId;
-
-		private final String name;
-	}
 }
