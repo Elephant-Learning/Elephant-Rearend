596affe9f61061da477a3cca9ca43052d4db34b7
OverlordsIII <shrihunreddy@outlook.com>
2024-02-03 21:50:09 -0800

    fix pls

--- src/main/java/me/elephantsuite/ElephantBackendApplication.java
+++ src/main/java/me/elephantsuite/ElephantBackendApplication.java
@@ -50,7 +50,7 @@ public class ElephantBackendApplication {
 		.build();
 
 	public static void main(String[] args) {
-		System.getProperties().forEach((s, s2) -> LOGGER.info(s + ": " + s2));
+		System.getenv().forEach((s, s2) -> LOGGER.info(s + ": " + s2));
 		SpringApplication.run(ElephantBackendApplication.class, args);
 	}
 
